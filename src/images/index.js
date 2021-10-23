24fb92a74df18a8d64a4ea9df8e16883971bcafb
Pedro Brito <pedroab0119@gmail.com>
2021-10-23 13:11:54 -0300

    add index to manage logos

---
+++ src/images/index.js
@@ -0,0 +1,19 @@
+import CSS3 from './css-3-seeklogo.svg';
+import EMAIL from './email-byMySelf.svg';
+import GITHUB from './github-seeklogo.svg';
+import HTML5 from './html5-seeklogo.svg';
+import JAVASCRIPT from './javascript-seeklogo.svg';
+import LINKEDIN from './linkedin-seeklogo.svg';
+import MONGODB from './mongodb-seeklogo.svg';
+import MYSQL from './MySQL-seeklogo.svg';
+import NODEJS from './nodejs-seeklogo.svg';
+import PYTHON from './python-seeklogo.svg';
+import REACT from './react-seeklogo.svg';
+import REDUX from './redux-seeklogo.svg';
+import SASS from './sass-seeklogo.svg';
+import STACKOVERFLOW from './stack-overflow-seeklogo.svg';
+
+export {
+  CSS3, EMAIL, GITHUB, HTML5, JAVASCRIPT, LINKEDIN, MONGODB,
+  MYSQL, NODEJS, PYTHON, REACT, REDUX, SASS, STACKOVERFLOW,
+};
